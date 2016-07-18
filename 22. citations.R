501cca1767a72f71612d92459c15962756453bc7
darokun <daloharodriguez@gmail.com>
2016-07-18 19:51:44 +0200

    finish background, methods and results. pending discussion and conclusions

---
+++ 22. citations.R
@@ -0,0 +1,26 @@
+# citations
+
+sessionInfo()
+
+citation()
+# R
+# R Core Team (2016). R: A language and environment for
+# statistical computing. R Foundation for Statistical
+# Computing, Vienna, Austria. URL https://www.R-project.org/
+
+citation("tmle")
+# tmle 
+# Susan Gruber, Mark J. van der Laan (2012). tmle: An R Package
+# for Targeted Maximum Likelihood Estimation. Journal of
+# Statistical Software, 51(13), 1-35. URL
+# http://www.jstatsoft.org/v51/i13/.
+
+citation("tmlecte")
+# tmlecte
+# Sam Lendle (2015). tmlecte: Estimates the NDE and ATT with TMLE. R package version 0.3.1.
+
+citation("SuperLearner")
+# SuperLearner
+# Eric Polley, Erin LeDell and Mark van der Laan (2016).
+# SuperLearner: Super Learner Prediction. R package version
+# 2.0-19. https://CRAN.R-project.org/package=SuperLearner
